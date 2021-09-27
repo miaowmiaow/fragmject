e1a02b5100344c5a592439520232b77019a38940
DESKTOP-PUGJUOL\Zhao <237934622@qq.com>
2021-09-27 22:46:44 +0800

    test

--- miaow-picture/src/main/java/com/example/miaow/picture/dialog/PictureBaseDialog.kt
+++ miaow-picture/src/main/java/com/example/miaow/picture/dialog/PictureBaseDialog.kt
@@ -1,13 +1,19 @@
 package com.example.miaow.picture.dialog
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Gravity
 import android.view.Window
 import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
 import com.example.fragment.library.base.R
 import com.example.fragment.library.base.dialog.BaseDialog
 
-open class PictureBaseDialog : BaseDialog() {
+open class PictureBaseDialog : DialogFragment() {
+
+    lateinit var manager: FragmentManager
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         dialog?.apply {
@@ -16,13 +22,48 @@ open class PictureBaseDialog : BaseDialog() {
         super.onActivityCreated(savedInstanceState)
         setStyle(STYLE_NORMAL, R.style.FullDialog)
         dialog?.window?.apply {
+            setDimAmount(0.5F)
             attributes.gravity = Gravity.BOTTOM
             decorView.setPadding(0, 0, 0, 0)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             setLayout(
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.MATCH_PARENT
             )
         }
+    }
+
+    override fun dismiss() {
+        try {
+            super.dismiss()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun show(manager: FragmentManager, tag: String?) {
+        if (!manager.isDestroyed && !manager.isStateSaved) {
+            try {
+                super.show(manager, tag)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun showNow(manager: FragmentManager, tag: String?) {
+        if (!manager.isDestroyed && !manager.isStateSaved) {
+            try {
+                super.showNow(manager, tag)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun show(manager: FragmentManager) {
+        this.manager = manager
+        show(manager, this::class.java.canonicalName)
     }
 
 }

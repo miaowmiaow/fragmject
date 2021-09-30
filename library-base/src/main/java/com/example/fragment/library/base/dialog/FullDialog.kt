30f1e40afcc2dd8979dc717e0af20d21b8109995
ZZZ <237934622@qq.com>
2021-09-30 13:32:15 +0800

    Test

--- library-base/src/main/java/com/example/fragment/library/base/dialog/FullDialog.kt
+++ library-base/src/main/java/com/example/fragment/library/base/dialog/FullDialog.kt
@@ -2,23 +2,26 @@ package com.example.fragment.library.base.dialog
 
 import android.os.Bundle
 import android.view.Gravity
-import android.view.Window
 import android.view.WindowManager
 import com.example.fragment.library.base.R
 
 class FullDialog : BaseDialog() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_FRAME, R.style.FullDialog)
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        dialog?.apply {
-            requestWindowFeature(Window.FEATURE_NO_TITLE)
-        }
         super.onActivityCreated(savedInstanceState)
-        setStyle(STYLE_NO_TITLE, R.style.FullDialog)
         dialog?.window?.apply {
             attributes.gravity = Gravity.BOTTOM
             decorView.setPadding(0, 0, 0, 0)
             setWindowAnimations(R.style.AnimBottom)
-            setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT)
+            setLayout(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT
+            )
         }
     }
 

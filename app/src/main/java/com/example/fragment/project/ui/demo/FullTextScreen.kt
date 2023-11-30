461563974bf06cc0fd848f11a852f5d208d1e16d
ZZZ <237934622@qq.com>
2023-11-30 17:21:55 +0800

    test

--- app/src/main/java/com/example/fragment/project/ui/demo/FullTextScreen.kt
+++ app/src/main/java/com/example/fragment/project/ui/demo/FullTextScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,10 +13,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.fragment.project.R
 import com.example.fragment.project.components.EllipsisText
 
@@ -32,6 +39,7 @@ fun FullTextScreen() {
             Box(modifier = Modifier.padding(10.dp)) {
                 EllipsisText(
                     text = buildAnnotatedString {
+                        appendInlineContent("icon")
                         append(
                             "壬戌之秋1，七月既望2，苏子与客泛舟游于赤壁之下。清风徐来3，水波不兴4。举酒属客5，诵明月之诗6，歌窈窕之章7。少焉8，月出于东山之上，徘徊于斗牛之间9。"
                         )
@@ -40,6 +48,24 @@ fun FullTextScreen() {
                     backgroundColor = colorResource(R.color.background),
                     fontSize = 14.sp,
                     maxLines = if (expand1) Int.MAX_VALUE else 2,
+                    inlineContent = mapOf(
+                        Pair(
+                            "icon",
+                            InlineTextContent(
+                                Placeholder(
+                                    width = 1.em,
+                                    height = 1.em,
+                                    placeholderVerticalAlign = PlaceholderVerticalAlign.TextCenter
+                                )
+                            ) {
+                                AsyncImage(
+                                    model = R.mipmap.avatar_1_raster,
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Fit,
+                                )
+                            }
+                        )
+                    ),
                     onTextLayout = {
                         ellipsis1 = it.isLineEllipsized(it.lineCount - 1)
                     },

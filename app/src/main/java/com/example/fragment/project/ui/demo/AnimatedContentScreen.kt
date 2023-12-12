fa1217cc6d29283801624b670284e25f10605ce5
ZZZ <237934622@qq.com>
2023-12-12 14:01:46 +0800

    test

--- app/src/main/java/com/example/fragment/project/ui/demo/AnimatedContentScreen.kt
+++ app/src/main/java/com/example/fragment/project/ui/demo/AnimatedContentScreen.kt
@@ -2,11 +2,14 @@ package com.example.fragment.project.ui.demo
 
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateIntOffsetAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -24,8 +27,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.example.fragment.project.R
@@ -77,7 +82,7 @@ fun AnimatedContentScreen() {
             ) {
                 moved = !moved
             }
-    ){
+    ) {
         Text("点我")
     }
 
@@ -97,4 +102,60 @@ fun AnimatedContentScreen() {
         // ...
     )
 
+    val refreshingResId = listOf(
+        R.mipmap.refreshing_1,
+        R.mipmap.refreshing_2,
+        R.mipmap.refreshing_3,
+        R.mipmap.refreshing_4,
+        R.mipmap.refreshing_5,
+        R.mipmap.refreshing_6,
+        R.mipmap.refreshing_7,
+        R.mipmap.refreshing_8,
+        R.mipmap.refreshing_9,
+        R.mipmap.refreshing_10,
+        R.mipmap.refreshing_11,
+        R.mipmap.refreshing_12,
+        R.mipmap.refreshing_13,
+        R.mipmap.refreshing_14,
+        R.mipmap.refreshing_15,
+        R.mipmap.refreshing_16,
+        R.mipmap.refreshing_17,
+        R.mipmap.refreshing_18,
+        R.mipmap.refreshing_19,
+        R.mipmap.refreshing_20,
+        R.mipmap.refreshing_21,
+        R.mipmap.refreshing_22,
+        R.mipmap.refreshing_23,
+        R.mipmap.refreshing_24,
+        R.mipmap.refreshing_25,
+        R.mipmap.refreshing_26,
+        R.mipmap.refreshing_27,
+        R.mipmap.refreshing_28,
+        R.mipmap.refreshing_29,
+        R.mipmap.refreshing_30,
+        R.mipmap.refreshing_31,
+        R.mipmap.refreshing_32,
+        R.mipmap.refreshing_33,
+        R.mipmap.refreshing_34,
+        R.mipmap.refreshing_35,
+        R.mipmap.refreshing_36,
+        R.mipmap.refreshing_37,
+    )
+    val refreshingInfiniteTransition = rememberInfiniteTransition(label = "SwipeRefresh")
+    val loadingAnimate by refreshingInfiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = refreshingResId.size.toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "loadingAnimate"
+    )
+    val id = loadingAnimate % refreshingResId.size
+    Image(
+        painter = painterResource(refreshingResId[id.toInt()]),
+        contentDescription = null,
+        modifier = Modifier.size(100.dp),
+        contentScale = ContentScale.Crop,
+    )
 }

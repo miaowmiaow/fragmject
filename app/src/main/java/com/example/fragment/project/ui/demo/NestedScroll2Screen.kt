1b4d2c9cbab709b32babee189de6a8427519dd8b
zzz <237934622@qq.com>
2026-05-06 15:47:28 +0800

    test

--- app/src/main/java/com/example/fragment/project/ui/demo/NestedScroll2Screen.kt
+++ app/src/main/java/com/example/fragment/project/ui/demo/NestedScroll2Screen.kt
@@ -1,45 +1,47 @@
 package com.example.fragment.project.ui.demo
 
 import android.annotation.SuppressLint
+import androidx.annotation.OptIn
 import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.media3.common.util.UnstableApi
 import com.example.fragment.project.WanTheme
 import com.example.fragment.project.components.SwipeRefreshBox
 import com.example.fragment.project.components.TabBar
 import kotlinx.coroutines.launch
 import kotlin.math.abs
 
+@OptIn(UnstableApi::class)
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun NestedScroll2Screen() {
@@ -54,8 +56,13 @@ fun NestedScroll2Screen() {
             add("item $i")
         }
     }
-    val targetHeight = 100.dp
-    val targetHeightPx = with(density) { targetHeight.roundToPx() }
+
+    var boxHeightDp by remember { mutableStateOf(0.dp) }
+    val topBarHeightDp = 100.dp
+    val topBarHeightPx = with(density) { topBarHeightDp.roundToPx() }
+    val tabBarHeightDp = 45.dp
+    val tabBarHeightPx = with(density) { tabBarHeightDp.roundToPx() }
+
     val targetPercent by remember { mutableStateOf(Animatable(1f)) }
 
     val tabs = listOf("tab1", "tab2")
@@ -70,7 +77,7 @@ fun NestedScroll2Screen() {
                 val delta = available.y
                 dyConsumed += delta
                 dyConsumed = dyConsumed.coerceAtMost(0f)
-                val percent = dyConsumed / targetHeightPx
+                val percent = dyConsumed / topBarHeightPx
                 scope.launch {
                     targetPercent.animateTo(1 - abs(percent.coerceIn(-1f, 0f)))
                 }
@@ -81,84 +88,82 @@ fun NestedScroll2Screen() {
             }
         }
     }
-    Scaffold(
-        modifier = Modifier.nestedScroll(nestedScrollConnection),
-        topBar = {
-            Column(
-                modifier = Modifier
-                    .background(WanTheme.theme)
-                    .fillMaxWidth()
-                    .requiredHeight(targetHeight * targetPercent.value)
-            ) {
-                Text(
-                    text = "嵌套滚动2",
-                    modifier = Modifier.offset {
-                        IntOffset(
-                            x = 0,
-                            y = -(targetHeightPx  * (1 - targetPercent.value)).toInt()
-                        )
-                    },
-                    fontSize = 16.sp,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer,
-                )
-                Text(
-                    text = "Tab + 下拉刷新",
-                    modifier = Modifier.offset {
-                        IntOffset(
-                            x = 0,
-                            y = -(targetHeightPx  * (1 - targetPercent.value)).toInt()
-                        )
-                    },
-                    fontSize = 32.sp,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer,
-                )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .onSizeChanged {
+                boxHeightDp = with(density) { it.height.toDp() }
             }
-        },
-        contentWindowInsets = WindowInsets.statusBars
-    ) { innerPadding ->
+            .nestedScroll(nestedScrollConnection),
+    ) {
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
+                .background(WanTheme.theme)
+                .fillMaxWidth()
+                .height(topBarHeightDp)
+                .graphicsLayer {
+                    translationY = -(topBarHeightPx * (1 - targetPercent.value))
+                },
+        ) {
+            Text(
+                text = "嵌套滚动2",
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+            )
+            Text(
+                text = "Tab + 下拉刷新",
+                fontSize = 32.sp,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(tabBarHeightDp)
+                .graphicsLayer {
+                    translationY = (topBarHeightPx * targetPercent.value)
+                }
         ) {
             TabBar(
+                onClick = { scope.launch { pagerState.animateScrollToPage(it) } },
                 data = tabs,
                 dataMapping = { it },
                 pagerState = pagerState,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(45.dp),
-                onClick = { scope.launch { pagerState.animateScrollToPage(it) } },
             )
-            HorizontalPager(state = pagerState) { _ ->
-                SwipeRefreshBox(
-                    items = data,
-                    isRefreshing = false,
-                    isLoading = false,
-                    isFinishing = true,
-                    onRefresh = {},
-                    onLoad = {},
+        }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .requiredHeight(boxHeightDp - tabBarHeightDp)
+                .graphicsLayer {
+                    translationY = tabBarHeightPx + (topBarHeightPx * targetPercent.value)
+                },
+        ) { _ ->
+            SwipeRefreshBox(
+                items = data,
+                isRefreshing = false,
+                isLoading = false,
+                isFinishing = true,
+                onRefresh = {},
+                onLoad = {},
+                key = { _, item -> item },
+            ) { _, item ->
+                Row(
                     modifier = Modifier
-                        .fillMaxSize(),
-                    key = { _, item -> item },
-                ) { _, item ->
-                    Row(
-                        modifier = Modifier
-                            .background(MaterialTheme.colorScheme.surfaceContainer)
-                            .fillMaxWidth()
-                            .padding(10.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                    ) {
-                        Text(
-                            text = item,
-                            fontSize = 14.sp,
-                            color = WanTheme.orange,
-                        )
-                    }
-                    HorizontalDivider()
+                        .background(MaterialTheme.colorScheme.surfaceContainer)
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text(
+                        text = item,
+                        fontSize = 14.sp,
+                        color = WanTheme.orange,
+                    )
                 }
+                HorizontalDivider()
             }
         }
-
     }
 }
